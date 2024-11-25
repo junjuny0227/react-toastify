0496c30900175490f0679af98d324a0820c0129f
junjuny0227 <junjuny0227@gmail.com>
2024-11-26 08:59:39 +0900

    many options

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-function App() {
-  const notify = () => toast("Wow so easy!");
+const App = () => {
+  const handleSuccess = () => toast.success("성공!");
+  const handleError = () => toast.error("에러 발생!");
+  const handleInfo = () => toast.info("정보 메시지");
+  const handleWarning = () => toast.warning("경고!");
 
   return (
     <div>
-      <button onClick={notify}>Notify!</button>
+      <button onClick={handleSuccess}>성공</button>
+      <button onClick={handleError}>에러</button>
+      <button onClick={handleInfo}>정보</button>
+      <button onClick={handleWarning}>경고</button>
       <ToastContainer />
     </div>
   );
-}
+};
 
 export default App;
